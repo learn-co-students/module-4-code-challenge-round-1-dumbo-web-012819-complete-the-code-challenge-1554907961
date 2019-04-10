19df962480e717d2a9e96f65aa978bfc0e785a43
github username <github email address>
2019-04-10 12:28:11 -0400

    figured out the conundrum. added more comments to isAlreadyExist function

--- src/App.js
+++ src/App.js
@@ -31,6 +31,12 @@ class App extends Component {
       // This is really concerning why this evaluates to true without comparing id values.
       // Should both of these reference the same object in memory?
       // That is a book from 'shelfBooks' compared to a book from 'books'?
+
+      // Oh silly me! in the addToBookshelf I add the bookObj I pass up straight into
+      // the copy array that would be set to shelfBooks. Hence why this works.
+      // Not sure if my way is good practice or the right way but to fix this I would
+      // simply copy the bookObj and its attributes like so: {...bookObj} and append
+      // that to the array. In turn I would change the line below to compare the id values.
       return book === bookObj
     })
   }
